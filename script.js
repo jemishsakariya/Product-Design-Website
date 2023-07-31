a85985d666b695d4d48ed680114eb667f72d5ad0
Jemish sakariya <74782154+jemishsakariya@users.noreply.github.com>
2023-07-31 20:46:44 +0530

    hero animation of heading using gsap

--- script.js
+++ script.js
@@ -12,3 +12,29 @@ function circleMouseFollower() {
 }
 
 circleMouseFollower();
+
+function heroAnimation() {
+  var tl = gsap.timeline();
+  tl.from("#nav", {
+    y: "-10",
+    opacity: 0,
+    ease: Expo.easeInOut,
+    duration: 1.5,
+  })
+    .to(".belem", {
+      y: 0,
+      ease: Expo.easeInOut,
+      duration: 2,
+      delay: -1,
+      stagger: 0.2,
+    })
+    .from("#herofooter", {
+      y: "-5",
+      opacity: 0,
+      duration: 1.5,
+      delay: -1.1,
+      ease: Expo.easeInOut,
+    });
+}
+
+heroAnimation();
