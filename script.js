226a47eac059694db56030f1cbab9ea05907ae68
Jemish sakariya <74782154+jemishsakariya@users.noreply.github.com>
2023-08-01 10:57:30 +0530

    second section animation done

--- script.js
+++ script.js
@@ -70,29 +70,29 @@ function heroAnimation() {
 
 heroAnimation();
 
-document.querySelectorAll(".elem").forEach(function (elem) {
-  var rotate = 0;
-  var diffrot = 0;
-  elem.addEventListener("mousemove", function (details) {
-    // console.log(elem.getBoundingClientRect());
-    var diff = details.clientY - elem.getBoundingClientRect().top;
-    diffrot = details.clientX - rotate;
-    rotate = details.clientX;
+// document.querySelectorAll(".elem").forEach(function (elem) {
+//   var rotate = 0;
+//   var diffrot = 0;
+//   elem.addEventListener("mousemove", function (details) {
+//     // console.log(elem.getBoundingClientRect());
+//     var diff = details.clientY - elem.getBoundingClientRect().top;
+//     diffrot = details.clientX - rotate;
+//     rotate = details.clientX;
 
-    gsap.to(elem.querySelector("img"), {
-      opacity: 1,
-      ease: Power3,
-      top: diff,
-      left: details.clientX,
-      rotate: gsap.utils.clamp(-20, 20, diffrot * 0.4),
-    });
-  });
+//     gsap.to(elem.querySelector("img"), {
+//       opacity: 1,
+//       ease: Power3,
+//       top: diff,
+//       left: details.clientX,
+//       rotate: gsap.utils.clamp(-20, 20, diffrot * 0.4),
+//     });
+//   });
 
-  elem.addEventListener("mouseleave", function () {
-    gsap.to(elem.querySelector("img"), {
-      opacity: 0,
-      ease: Power3,
-      duration: 0.2,
-    });
-  });
-});
+//   elem.addEventListener("mouseleave", function () {
+//     gsap.to(elem.querySelector("img"), {
+//       opacity: 0,
+//       ease: Power3,
+//       duration: 0.2,
+//     });
+//   });
+// });
